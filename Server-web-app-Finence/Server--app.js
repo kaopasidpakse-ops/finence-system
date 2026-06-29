be87b583ebac675e5a1ec5ada7a8947e17868c11
keopashit <kaopasidpakse@gmail.com>
2026-06-29 21:51:45 +0700

    add PORT

--- Server-web-app-Finence/Server--app.js
+++ Server-web-app-Finence/Server--app.js
@@ -23,8 +23,8 @@ app.use(express.json());
 
 
 app.use('/api', router);
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000'); 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`); 
 });
